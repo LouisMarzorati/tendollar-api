17999b6956c9661f7756894b6c519a1122f646c3
Louis Marzorati <LouisMarzorati@users.noreply.github.com>
2022-04-09 19:38:49 -0400

    init everything

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
---
+++ config/plugins.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "hi@tendollar.site",
+        defaultReplyTo: "hi@tendollar.site",
+      },
+    },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: "tendollar-home",
+        },
+      },
+    },
+  },
+});
